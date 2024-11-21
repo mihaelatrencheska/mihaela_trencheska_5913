e5505a96007fcb67c1cd46ecc0ef9244457f7ee5
student <student@uacs.mk>
2024-11-21 18:08:32 +0100

    Lecture 8 ID5913

---
+++ Lecture08/Excercise01_Reflection/src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    private String s;
+
+    public Test() {
+        s = "Programming languages";
+    }
+
+    public void method1() {
+        System.out.println("Value of s is : " + s);
+    }
+
+    public void method2() {
+        System.out.println("Value of s is: " + s);
+
+    }
+    public void method3() {
+        System.out.println("Invoke the value of s :  " + s);
+
+    }
+}
+
